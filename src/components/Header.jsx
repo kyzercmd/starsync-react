5acf5ef29ed1c8192a219e8cf22c6bf84e085c20
Kaiser <Kaiser@DESKTOP-8SV52KQ>
2025-11-18 13:37:59 +0600

    save data on localstorage and themes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from "react";
+
 import Logo from "../assets/logo.png";
 
 export const Header = () => {
+  const [theme, setTheme] = useState(
+    JSON.parse(localStorage.getItem("theme")) || "default"
+  );
+
+  useEffect(() => {
+    localStorage.setItem("theme", JSON.stringify(theme));
+    document.documentElement.removeAttribute("class");
+    document.documentElement.classList.add(theme);
+  }, [theme]);
+
   return (
     <header>
       <div className="logo">
@@ -8,7 +20,14 @@ export const Header = () => {
         <span>StarSync - Align Your Tasks</span>
       </div>
       <div className="themeSelector">
-        <span className="light"></span>
+        <span
+          onClick={() => setTheme("default")}
+          className={theme === "default" ? "default activeTheme" : "default"}
+        ></span>
+        <span
+          onClick={() => setTheme("light")}
+          className={theme === "light" ? "light activeTheme" : "light"}
+        ></span>
       </div>
     </header>
   );
